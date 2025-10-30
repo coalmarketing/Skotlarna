f3c7dd21c51fd0ece318188fdd68310bae4ed8ec
hana-cas <hanacaslavkova@gmail.com>
2025-10-30 16:27:14 +0100

    30.10. 2

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
